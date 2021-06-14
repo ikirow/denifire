b6910eed4417f96c41f935a754ec5985fbeed4fb
Alexander Petrov <alex.petrov.snade@gmail.com>
2021-06-14 19:54:40 +0300

    disable dropdown functionality - needs to be refactored to target only items with sub-categories and to allow click on the main category with explan only when clicking on the plus sign(and making it with 30x30 touch friendly hitbox)

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,20 +2,18 @@
 	'use strict';
 
 	$( function () {
-
+		/*
 		// JS
 		let categoryDropDownTrigger = $('#woocommerce_product_categories-2 .cat-item')
 		categoryDropDownTrigger.click(function( event ) {
-			console.log(event.currentTarget)
-			event.preventDefault();
-			
+			//console.log(event.currentTarget)
 			if($(event.currentTarget).children('.children').length > 0){
 				event.preventDefault();
 				$(event.currentTarget).find('.children').toggleClass('active');
 				$(event.currentTarget).toggleClass('active');
 			}				
 		  });
-
+		*/
 	} );
 
 
