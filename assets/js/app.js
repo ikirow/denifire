691cc4f5d832dc52c1714088453e03e1ffa76cb9
Ivan Kirov <ikirow@gmail.com>
2023-04-18 16:04:00 +0300

    latest updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,22 +3,25 @@
 
 	$( function () {
 		// JS
+		const showMoreBtn = $("<div class='show-more-btn'>+</div>")
 		let categoryDropDownTrigger = $('#woocommerce_product_categories-2 .cat-item')
 
+
 		$( window ).load(function() {
-			$('.current-cat').find('.children').toggleClass('active');
+			$('.cat-parent').append(showMoreBtn);
+			// $('.current-cat').find('.children').toggleClass('active');
 			$('.current-cat > a').toggleClass('active');
 		  });
 		  
-		categoryDropDownTrigger.click(function( event ) {
+		  showMoreBtn.click(function( event ) {
 			
 			if($(event.target).parent().children('.children').length > 0 && ($(event.target).is(".cat-parent") || $(event.target).parent().hasClass("cat-parent") )){
 			
 				if(!$(event.target).hasClass('active')){
 					event.preventDefault();
 					event.stopPropagation();
-					$(event.target).parent().find('.children').toggleClass('active');
-					$(event.target).toggleClass('active');
+					$(event.target).parent().children('.children').toggleClass('active');
+					$(event.target).parent().children('a').toggleClass('active');
 				}
 				
 			}				
